753eac7281095afddd34dd708c09d947fa3ca6f0
Yongjae Park <mapmatters.park@gmail.com>
2019-09-18 08:35:59 +0900

    Add files via upload

---
+++ insta4.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup as bs
+import time
+import re
+from urllib.request import urlopen
+import json
+from pandas.io.json import json_normalize
+import pandas as pd, numpy as np
+import pprint
+import pickle 
+
+with open ('links_list', 'rb') as fp:
+    links = pickle.load(fp)
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(links)
+
+page = urlopen(links[0]).read()
+data=bs(page, 'html.parser')
+body = data.find('body')
+script = body.find('script')
+raw = script.text.strip().replace('window._sharedData =', '').replace(';', '')
+json_data=json.loads(raw)
+posts =json_data['entry_data']['PostPage'][0]['graphql']
+posts= json.dumps(posts)
+posts = json.loads(posts)
+x = pd.DataFrame.from_dict(json_normalize(posts), orient='columns') 
+x.columns =  x.columns.str.replace("shortcode_media.", "")
+pp.pprint(posts) 
